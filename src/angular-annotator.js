5a312b1b700b29e5ad633ef1a1fafda0e6bb01bb
mwasiluk <m.wasiluk@sages.com.pl>
2014-12-04 18:01:27 +0100

    initial commit

---
+++ src/angular-annotator.js
@@ -0,0 +1,43 @@
+angular.module('mwAnnotator', []);
+
+angular.module('mwAnnotator').directive('mwAnnotator', function () {
+
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var options = {};
+            if(attrs.mwAnnotator){
+                options = scope.$eval(attrs.mwAnnotator);
+            }
+
+            var annotator = angular.element(element).annotator(options).data('annotator');
+            annotator.addPlugin('Unsupported');
+
+            if(attrs.annotatorTags){
+                annotator.addPlugin('Tags');
+            }
+
+            if(attrs.annotatorPermissions){
+                var permissionsOptions = scope.$eval(attrs.annotatorPermissions);
+                annotator.addPlugin('Permissions', permissionsOptions);
+            }
+
+            if(attrs.annotatorStore){
+                var storeOptions = scope.$eval(attrs.annotatorStore);
+                annotator.addPlugin('Store', storeOptions);
+            }
+
+            if(attrs.annotatorFilter){
+                var filterOptions = scope.$eval(attrs.annotatorStore);
+                annotator.addPlugin('Filter', filterOptions);
+            }
+
+
+            if(attrs.annotatorMarkdown){
+                annotator.addPlugin('Markdown');
+            }
+
+
+        }
+    };
+});
